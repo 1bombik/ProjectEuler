ad85ce9415f52f6060c0c439c76030e56a9335e1
1bombik <dima.vezh14@mail.ru>
2022-12-18 01:55:41 +0300

    Some optimizations

--- eiler_12.py
+++ eiler_12.py
@@ -1,8 +1,10 @@
+# https://projecteuler.net/problem=12
+
 def foo():
     nums = []
     count = 0
     num = 0
-    for tr_num in range(1, 100_000_000):
+    for tr_num in range(1, 12500):
         while count <= tr_num:
             num += count
             count += 1
@@ -11,20 +13,14 @@ def foo():
 
 
 def foo1(nums):
-    div_len = 0
     for number in nums:
-        divs = []
-        for i in range(1, number+1):
-            if number % i == 0:
-                divs.append(i)
-        if len(divs) > div_len:
-            div_len = len(divs)
-            if len(divs) > 500:
-                answer_num = number
-                answer_dividers = divs
-                break
-    return (f'{answer_num}\n'
-            f'{len(answer_dividers)}')
+        divs = [i for i in range(1, number+1) if number % i == 0]
+        if len(divs) > 500:
+            answer_num = number
+            answer_dividers = divs
+            break
+    return (f'Answer - {answer_num}\n'
+            f'Quantity of dividers - {len(answer_dividers)}')
 
 
 print(foo1(foo()))
