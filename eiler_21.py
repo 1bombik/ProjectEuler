10ed4067281a1b0033e101425336e2f06e7ff999
1bombik <dima.vezh14@mail.ru>
2023-01-05 20:55:53 +0300

    Initial commit

---
+++ eiler_21.py
@@ -0,0 +1,15 @@
+answer = []
+for num in range(1, 10000):
+    friend_num = num
+    da = 0
+    db = 0
+    for div in range(1, friend_num):
+        if friend_num % div == 0:
+            da += div
+    for divs in range(1, da):
+        if da % divs == 0:
+            db += divs
+    if friend_num == db and da != db:
+        answer.append(da)
+        answer.append(db)
+print(sum(set(answer)))
